b9ea09fcd8dcee330f9c0d88561a82687debb00b
John Doe <johndoe@example.com>
2017-04-30 15:35:42 +0530

    button fix

--- app/src/main/java/com/example/dell/inheritenceexample/MainActivity.java
+++ app/src/main/java/com/example/dell/inheritenceexample/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.dell.inheritenceexample;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends BaseActivity {
@@ -12,6 +14,16 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button= (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(getApplicationContext(),SecondActivity.class));
+
+
+            }
+        });
 
     }
 
